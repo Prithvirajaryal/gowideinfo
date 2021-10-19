e015052879bc06c6ecd86be2547d283b34ed7f69
Prithvirajaryal <59389373+Prithvirajaryal@users.noreply.github.com>
2021-10-19 08:30:58 +0545

    Update gowide.js

--- gowide.js
+++ gowide.js
@@ -4,7 +4,7 @@ const person = {
   gender: 'male',
   interest: ['travel', 'reading'],
   bio: function() {
-    alert (this.name[0] + this.name[1] + " is" + this.age + " years" + this.gender + "." + " He likes " + this.interest[0] + " and " + this.interest[1] + ".");    
+    alert (this.name[0] + ' ' + this.name[1] + " is" + this.age + " years" + this.gender + "." + " He likes " + this.interest[0] + " and " + this.interest[1] + ".");    
   },
   greeting: function() {
     alert ("Hi, I\'m' " + this.name[0] + "."); 
